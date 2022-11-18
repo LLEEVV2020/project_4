0ad19215d75ed7a8a98ae8c5a5f55466be9691ed
sergeyFrontend <utemishevsergey02@gmail.com>
2022-11-18 18:14:35 +0500

    add maybe

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
+import Swiper from 'swiper'
+import 'swiper/'
+import 'swiper/css';
 import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
+
+
+
+
+const swiper = new Swiper('.header-swiper',{
+  direction: 'vertical',
+  loop: true,
+
+});
