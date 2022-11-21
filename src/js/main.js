14bb86b12f6134d4f7f1421443695ed2a1d551ee
sergeyFrontend <utemishevsergey02@gmail.com>
2022-11-21 21:22:48 +0500

    add logic for modal-window

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
-const swiper = new Swiper('.slider-container',{
- autoplay: {
+const swiper = new Swiper(".slider-container", {
+  autoplay: {
     delay: 3500,
     disableOnInteraction: false,
   },
   speed: 3100,
   pagination: {
     el: ".swiper-p1",
-    clickable: true
+    clickable: true,
   },
   navigation: {
     nextEl: ".swiperbn1",
@@ -14,8 +14,24 @@ const swiper = new Swiper('.slider-container',{
   },
 });
 
+// modal window
+const overlayModal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".modal__close");
+const openBtn = document.querySelectorAll('.');
 
-// burger-menu
-const btn = document.querySelector('.header__menu-btn');
+//закрытие модалки
+overlayModal.addEventListener("click", ({ target }) => {
+  if (target === overlayModal || target.closest(".modal__close")) {
+    overlayElem.remove();
+  }
+});
+
+for (let i = 0; i < openBtn.length; i++) {
+  openBtn[i].addEventListener("click", () => {
+     overlayModal.classList.toggle('modal--active');
+  });
+}
+
+// открытие модалки по кнопки
 
 //new WOW().init();
