eec68693e6bc4e7584924d1bd142df1f19616f34
lleevv2030@gmail.com <lleevv2020@mail.com>
2022-11-29 23:49:30 +0400

    add

--- src/js/main.js
+++ src/js/main.js
@@ -109,11 +109,11 @@ modalController({
 Скрипт*/
 const filterBox = document.querySelectorAll(".box");
 const navBlock = document.querySelector(".swiper-wrapper");
-console.log(navBlock);
+//console.log(navBlock);
 navBlock.addEventListener("click", (event) => {
   if (event.target.className !== "swiper-slide__img") return false;
   let filterClass = event.target.dataset["f"];
-  console.log(filterClass);
+  //console.log(filterClass);
   filterBox.forEach((elem) => {
     let item_img = elem.querySelector("img");
     if (item_img !== null) {
@@ -123,7 +123,7 @@ navBlock.addEventListener("click", (event) => {
     $img.src = elem.dataset["background_image"];
     elem.appendChild($img);
     let backgroundImage = elem.dataset["background_image"];
-    console.log(backgroundImage);
+    //console.log(backgroundImage);
     elem.style.backgroundImage = backgroundImage;
     elem.classList.remove("hide");
     if (!elem.classList.contains(filterClass) && filterClass !== "all") {
@@ -140,3 +140,166 @@ burgerBtn.addEventListener("click", ({target}) => {
   overtlayMenu.classList.toggle("header-top__inner--active");
 
 });
+
+
+
+// email
+
+
+// телефон заполнить полностью
+let phoneText = false;
+// емайл заполнить полностью
+let emailText = false;
+
+var h_form_btn = document.querySelectorAll('.btn-send');
+
+h_form_btn.forEach(function (item) {
+    formReadyClick(item);
+});
+
+function formReadyClick (btn) {
+
+    let form = btn.closest("form");
+
+    form.addEventListener("submit", formSendAsync2, {once: false});
+
+    async function formSendAsync2(e){
+
+        e.preventDefault(); // запрет на отправку стандартной формы
+
+        formSendAsync(btn);
+    }
+}
+
+
+
+async function formSendAsync(btn_child){
+
+    let form = btn_child.closest("form");
+
+    let error = formValidate(form);
+
+    let formData = new FormData(form);
+
+    if(error === 0){
+
+        form.classList.add('_sending');
+        let response = await fetch('../callback-request/index.php', {
+           method: 'POST',
+            body: formData
+        });
+        if(response.ok){
+            let result = await response.json();
+
+            form.reset();
+            form.classList.remove('_sending');
+
+            return true;
+
+        } else {
+            alert("ошибка");
+        }
+
+    } else {
+        if(phoneText){
+            alert('Введите ещё цифры телефона');
+        } else if(emailText)  {
+            alert('Введите емайл полностью');
+        } else {
+            alert('заполните поля');
+        }
+    }
+    return false;
+}
+
+// проверка на ошибки
+function formValidate(form){
+    let error = 0;
+
+    let formReq = form.getElementsByClassName('_req');
+
+    for (let index = 0; index < formReq.length; index++){
+        const input = formReq[index];
+        formRemoveError(input); // убрать класс проверки
+
+        if(input.classList.contains('_email')){
+            if(emailTest(input)){
+                formAddError(input);
+                error++;
+            }
+        }
+        else if(input.classList.contains('input__mask')){
+            if(phoneTest(input)){
+                formAddError(input);
+                error++;
+                phoneText = true;
+            }
+        }
+        else if(input.getAttribute("type") === "checkbox" && input.checked === false){
+            formAddError(input);
+            error++;
+        } else {
+            if(input.value === ''){
+                formAddError(input);
+                error++;
+            }
+        }
+    }
+    return error;
+}
+// добавляют родители и элементу класс _error
+function formAddError(input){
+    input.parentElement.classList.add('_error');
+    input.classList.add('_error');
+}
+function formRemoveError(input){
+    input.parentElement.classList.remove('_error');
+    input.classList.remove('_error');
+}
+
+// проверка emai
+function emailTest(input){
+    emailText = true;
+    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
+
+// проверка телефона
+function phoneTest(input){
+    if(input.value.replace(/ +/g, ' ').trim() === "+7" ||
+       input.value === '' ||
+       input.value.replace(/[\+\(\)\s]/g,"").length < 10
+      ){
+
+       return true ;
+    }
+
+    return false;
+}
+
+  const formImage = document.getElementById('quiz__file');
+  const formPreview = document.querySelector('.input__file-button-text');
+
+  if(formImage){
+      formImage.addEventListener('change', () => {
+          uploadFile(formImage.files[0]);
+      });
+  }
+
+function uploadFile(file){
+
+    if(file.size > 4 * 1024 * 1024){
+        alert('файл должен быть менее 4мб');
+        return;
+    }
+
+    var reader = new FileReader();
+
+    reader.onload = function (e){
+        formPreview.innerHTML = `<span style="color: #f28d55;">Файл добавлен</span>`;
+    }
+    reader.onerror = function (e){
+        alert('Ошибка');
+    };
+    reader.readAsDataURL(file);
+
+}
